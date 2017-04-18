a8f96f84363c4d64ed964583cd746f05f634ab12
Dinh Le <dinh.v.le@gmail.com>
2017-04-18 12:32:15 -0500

    refactor, it's better to use functional component instead of class if you are not planning to use state.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,24 +7,106 @@ var style = {
   color: 'rebeccapurple'
 };
 
-var HomePage = function() {
-  return (
-    <div>
-      <h1 style={style}>
-        Welcome to Cat and Dog Cuteness Fight Game!!!
-      </h1>
-      <div style={{marginTop: 60, textAlign: 'center'}}>
-        <PetComponent
-          petName="Cat"
-          petImageUrl="https://i.redd.it/qh713wbo4r8y.jpg"
-        />
-        <PetComponent
-          petName="Dog"
-          petImageUrl="https://s-media-cache-ak0.pinimg.com/originals/5e/0b/13/5e0b1350965d186b299cdd457a32aa50.jpg"
-        />
+var btnStyle = {
+  marginTop: '30px',
+  marginRight: '5px',
+  height: '25px'
+}
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      catLikesCount: 0,
+      dogLikesCount: 0
+    };
+    this.handleShowWinnerBtnClick = this.handleShowWinnerBtnClick.bind(this);
+    this.handleStartOverBtnClick = this.handleStartOverBtnClick.bind(this);
+    this.handleDislikeBtnClick = this.handleDislikeBtnClick.bind(this);
+    this.handleLikeBtnClick = this.handleLikeBtnClick.bind(this);    
+  }
+  handleLikeBtnClick(event) {
+    var petName = event.target.value;
+
+    if (petName === 'Cat') {
+      this.setState(function(prevState) {
+        return {
+          catLikesCount: prevState.catLikesCount + 1,
+          dogLikesCount: prevState.dogLikesCount
+        }
+      })
+    } else if (petName === 'Dog') {
+      this.setState(function(prevState) {
+        return {
+          catLikesCount: prevState.catLikesCount,
+          dogLikesCount: prevState.dogLikesCount + 1
+        }
+      })
+    }
+  }
+  handleDislikeBtnClick(event) {
+    var petName = event.target.value;
+
+    if (petName === 'Cat') {
+      this.setState(function(prevState) {
+        return {
+          catLikesCount: prevState.catLikesCount - 1,
+          dogLikesCount: prevState.dogLikesCount
+        }
+      })
+    } else if (petName === 'Dog') {
+      this.setState(function(prevState) {
+        return {
+          catLikesCount: prevState.catLikesCount,
+          dogLikesCount: prevState.dogLikesCount - 1
+        }
+      })
+    }
+  }  
+  handleShowWinnerBtnClick() {
+    var catLikesCount = this.state.catLikesCount;
+    var dogLikesCount = this.state.dogLikesCount;
+    
+    if (catLikesCount > dogLikesCount) {
+      console.log('Cat is the Winner');
+    } else if (catLikesCount < dogLikesCount) {
+      console.log('Dog is the Winner');
+    } else {
+      console.log('It\'s a tie');
+    }
+  }
+  handleStartOverBtnClick() {
+    
+  }
+  render() {
+    return (
+      <div>
+        <h1 style={style}>
+          Welcome to Cat and Dog Cuteness Fight Game!!!
+        </h1>
+        <div style={{marginTop: 60, textAlign: 'center'}}>
+          <PetComponent
+            petName="Cat"
+            likesCount={this.state.catLikesCount}
+            petImageUrl="https://i.redd.it/qh713wbo4r8y.jpg"
+            onLikeBtnClick={this.handleLikeBtnClick}
+            onDislikeBtnClick={this.handleDislikeBtnClick}
+          />
+          <PetComponent
+            petName="Dog"
+            likesCount={this.state.dogLikesCount}
+            petImageUrl="https://s-media-cache-ak0.pinimg.com/originals/5e/0b/13/5e0b1350965d186b299cdd457a32aa50.jpg"
+            onLikeBtnClick={this.handleLikeBtnClick}
+            onDislikeBtnClick={this.handleDislikeBtnClick}
+          />
+        </div>
+        <div style={{textAlign: 'center'}}>
+          <button style={btnStyle} onClick={this.handleShowWinnerBtnClick}>Show Winner</button>
+          <button style={btnStyle} onClick={this.handleStartOverBtnClick}>Start Over</button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );    
+  }
+}
 
 module.exports = HomePage;
--- src/components/PetComponent.js
+++ src/components/PetComponent.js
@@ -14,42 +14,16 @@ var btnStyle = {
   marginRight: '5px'
 };
 
-class PetComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      likesCount: 0
-    };
-    this.handleDislikeBtnClick = this.handleDislikeBtnClick.bind(this);
-    this.handleLikeBtnClick = this.handleLikeBtnClick.bind(this);
-  }
-  handleLikeBtnClick() {
-    this.setState(function(prevState) {
-      return {
-        likesCount: prevState.likesCount + 1
-      }
-    });
-  }
-  handleDislikeBtnClick() {
-    this.setState(function(prevState) {
-      return {
-        likesCount: prevState.likesCount - 1
-      }
-    });
-  }
-
-  render() {
-    console.log('Inside render method: ', this.state.likesCount);
-    return (
-      <div style={comStyle}>
-        <h3>{this.props.petName} Likes: {this.state.likesCount}</h3>
-        <img style={{height: 400, width:400}} src={this.props.petImageUrl} alt={"Cute " + this.props.petName}/>
-        <br />
-        <button style={btnStyle} onClick={this.handleLikeBtnClick}>Like</button>
-        <button style={btnStyle} onClick={this.handleDislikeBtnClick}>Dislike</button>
-      </div>
-    );
-  }
+var PetComponent = function(props) {
+  return (
+    <div style={comStyle}>
+      <h3>{props.petName} Likes: {props.likesCount}</h3>
+      <img style={{height: 400, width:400}} src={props.petImageUrl} alt={"Cute " + props.petName}/>
+      <br />
+      <button style={btnStyle} value={props.petName} onClick={props.onLikeBtnClick}>Like</button>
+      <button style={btnStyle} value={props.petName} onClick={props.onDislikeBtnClick}>Dislike</button>
+    </div>
+  );  
 }
 
 module.exports = PetComponent;
